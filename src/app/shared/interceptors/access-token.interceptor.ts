bc3990c3e46c0bcdaf98b79d4c0808705ad2a996
Chris Giannakopoulos <chris.giannakopoulos.dev@gmail.com>
2024-05-04 23:19:53 +0300

    fix: types

--- src/app/shared/interceptors/access-token.interceptor.ts
+++ src/app/shared/interceptors/access-token.interceptor.ts
@@ -9,19 +9,25 @@ import { Observable } from 'rxjs';
 import { AngularFireAuth } from "@angular/fire/compat/auth";
 import { switchMap, take } from "rxjs/operators";
 
+
+interface Request {
+  url: string;
+  method: string;
+}
+
 @Injectable()
 export class AccessTokenInterceptor implements HttpInterceptor {
 
   constructor(private angularAuth: AngularFireAuth) {
   }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(request: HttpRequest<Request>, next: HttpHandler): Observable<HttpEvent<Request>> {
     return this.angularAuth.idToken.pipe(
       take(1),
-      switchMap((token: any) => {
+      switchMap((token: string | null) => {
         if (token) {
           request = request.clone({
-            setHeaders: {Authorization: `${token}`}
+            setHeaders: { Authorization: `${token}` }
           });
         }
         return next.handle(request);
