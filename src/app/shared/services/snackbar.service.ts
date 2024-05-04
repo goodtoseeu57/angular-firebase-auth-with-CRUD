5d952a37afb1f9a34e1ee1d2e8c51017274e8a77
Chris Giannakopoulos <chris.giannakopoulos.dev@gmail.com>
2024-05-04 23:19:59 +0300

    fix: types

--- src/app/shared/services/snackbar.service.ts
+++ src/app/shared/services/snackbar.service.ts
@@ -4,18 +4,20 @@ import { MatSnackBar } from "@angular/material/snack-bar";
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class SnackbarService {
 
   constructor(private snackbar: MatSnackBar) {
   }
 
-  showSnackBar(message: string, cssClass: any) {
+  showSnackBar(message: string, cssClass: string) {
     this.snackbar.open(message, '', {
-        duration: 3000,
-        verticalPosition: 'top',
-        horizontalPosition: 'center',
-        panelClass: [cssClass]
-      }
+      duration: 3000,
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+      panelClass: [cssClass]
+    }
     );
   }
 }
